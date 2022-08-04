c05b470f899a3bba134f42d3774801cdd271d5e4
Rebecca Buehler <you@example.com>
2022-08-04 14:15:39 -0700

    adding a function to help with account withdrawal

--- atm-account.jsx
+++ atm-account.jsx
@@ -1,37 +1,72 @@
 // this keeps a running total of deposits and withdrawals
 
-const ATMDeposit = ({ onChange, isDeposit }) => {
+const ATMDeposit = ({ onChange, isDeposit, isValid }) => {
   const choice = ["Deposit", "Cash Back"];
   return (
     <label className="label huge">
       <h3>{choice[Number(!isDeposit)]}</h3>
-      <input type="number" width="200" onChange={onChange}></input>
-      <input type="submit" width="200" value="Submit"></input>
+      <input id="number-input" type="number" width="200" onChange={onChange}></input>
+      <input type="submit" disabled={!isValid} width="200" value="Submit" id="submit-input"></input>
     </label>
   );
 };
 
 const Account = () => {
-  var deposit = 0;
+  const [deposit, setDeposit] = React.useState(0);
   const [accountState, setAccountState] = React.useState(0);
   const [isDeposit, setIsDeposit] = React.useState(true);
+  const [atmMode, setAtmMode] = React.useState("");
+  const [validTransaction, setValidTransaction] = React.useState(false);
 
   const handleChange = (event) => {
-    console.log(`handleChange ${event.target.value}`);
-    deposit = Number(event.target.value);
+    console.log(Number(event.target.value));
+    if (Number(event.target.value) <= 0) {
+      return setValidTransaction(false);
+    }
+    if (atmMode === "Cash Back" && Number(event.target.value) > accountState) {
+      setValidTransaction(false);
+    } else {
+      setValidTransaction(true);
+    }
+    setDeposit(Number(event.target.value));
   };
   const handleSubmit = (event) => {
     let newTotal = isDeposit ? accountState + deposit : accountState - deposit;
     setAccountState(newTotal);
+    setValidTransaction(false);
     event.preventDefault();
+  };
+  const handleModeSelect = (event) => {
+    console.log(event.target.value);
+    setAtmMode(event.target.value);
+    setValidTransaction(false);
+    if (event.target.value === "Deposit") {
+      setIsDeposit(true);
+    } else {
+      setIsDeposit(false);
+    }
   };
 
   return (
     <form onSubmit={handleSubmit}>
       <h2>Account Balance {accountState}</h2>
-      <button onClick={() => setIsDeposit(true)}>Deposit</button>
-      <button onClick={() => setIsDeposit(false)}>Cash Back</button>
-      <ATMDeposit onChange={handleChange} isDeposit={isDeposit}></ATMDeposit>
+      <label>Select an action below to continue</label>
+        <select onChange={(e) => handleModeSelect(e)} name="mode" id="mode-select">
+          <option id="no-selection" value=""></option>
+          <option id="deposit-selection" value="Deposit">
+            Deposit
+          </option>
+          <option id="cashback-selection" value="Cash Back">
+            Cash Back
+          </option>
+        </select>
+        {atmMode && (
+          <ATMDeposit
+            onChange={handleChange}
+            isDeposit={isDeposit}
+            isValid={validTransaction}
+          ></ATMDeposit>
+        )}
     </form>
   );
 };
@@ -40,4 +75,3 @@ ReactDOM.render(<Account />, document.getElementById("root"));
 
 // Can you create functionality so users can’t withdraw more money than they have in their account balance?
 // Are there any updates in the user interface that you could change?
-//add a feature to prevent cash back greater than account balance
