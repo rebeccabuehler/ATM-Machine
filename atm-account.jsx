5a176909c039758061308510bbd041a7a4f4f465
Rebecca Buehler <you@example.com>
2022-08-04 13:34:19 -0700

    Adding buttons for deposit and cash back, making a function to tell whether it is deposit or cash back

--- atm-account.jsx
+++ atm-account.jsx
@@ -1,36 +1,40 @@
 // this keeps a running total of deposits and withdrawals
 
-const ATMDeposit = ({ onChange }) => {
-    return (
-      <label className="label huge">
-        Deposit:
-        <input type="number" onChange={onChange}></input>
-        <input type="submit"></input>
-      </label>
-    );
+const ATMDeposit = ({ onChange, isDeposit }) => {
+  const choice = ["Deposit", "Cash Back"];
+  return (
+    <label className="label huge">
+      <h3>{choice[Number(!isDeposit)]}</h3>
+      <input type="number" width="200" onChange={onChange}></input>
+      <input type="submit" width="200" value="Submit"></input>
+    </label>
+  );
+};
+
+const Account = () => {
+  var deposit = 0;
+  const [accountState, setAccountState] = React.useState(0);
+  const [isDeposit, setIsDeposit] = React.useState(true);
+
+  const handleChange = (event) => {
+    console.log(`handleChange ${event.target.value}`);
+    deposit = Number(event.target.value);
   };
-  
-  const Account = () => {
-    const [accountState, setAccountState] = React.useState(0);
-    var deposit = 0;
-    const handleChange = event => {
-      console.log(`handleChange ${event.target.value}`);
-      deposit = Number(event.target.value);
-    };
-    const handleSubmit = event => {
-      let newTotal = accountState + deposit;
-      alert(`Account total = ${newTotal}`);
-      setAccountState(newTotal);
-      event.preventDefault();
-    };
-  
-    return (
-      <form onSubmit={handleSubmit}>
-        <h2>Account Balance {accountState}</h2>
-        <ATMDeposit onChange={handleChange}> Deposit</ATMDeposit>
-      </form>
-    );
+  const handleSubmit = (event) => {
+    let newTotal = isDeposit ? accountState + deposit : accountState - deposit;
+    alert(`Account total = ${newTotal}`);
+    setAccountState(newTotal);
+    event.preventDefault();
   };
-  // ========================================
-  ReactDOM.render(<Account />, document.getElementById("root"));
-  
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Account Balance {accountState}</h2>
+      <button onClick={() => setIsDeposit(true)}>Deposit</button>
+      <button onClick={() => setIsDeposit(false)}>Cash Back</button>
+      <ATMDeposit onChange={handleChange} isDeposit={isDeposit}></ATMDeposit>
+    </form>
+  );
+};
+// ========================================
+ReactDOM.render(<Account />, document.getElementById("root"));
